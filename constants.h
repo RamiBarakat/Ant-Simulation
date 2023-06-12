25f5c0f3ce67be3be2f1c860d5d6575b0596efc6
RamiBarakat <ramibarakat522002@gmail.com>
2023-06-12 18:38:55 +0300

    First

--- constants.h
+++ constants.h
@@ -2,15 +2,6 @@
 #define __CONSTANTS__
 
 #define MAX_MSG_SIZE 100
-#define SEM_SEED 1234
-#define MEM_SEED 2314
-#define DEFAULT_HELPER_COUNT 2
-#define DEFAULT_SPIES_COUNT 2
 
-struct message
-{
-    long type;               // Message type
-    char text[MAX_MSG_SIZE]; // Message data
-};
 
 #endif
